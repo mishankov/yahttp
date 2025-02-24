f992685f96e29698ddb05f5166114710a3e030eb
Pierre Guillemot <pierre@kalvad.com>
2025-02-24 16:04:49 +0400

    Json unit tests

--- tests/unit/test_yahttp.nim
+++ tests/unit/test_yahttp.nim
@@ -24,3 +24,43 @@ test "Convert to object":
     key: string
 
   check Response(status: 200, body: "{\"key\": \"value\"}").to(T) == T(key: "value")
+
+test "Parsing an object into a string":
+  type MyJson = object
+    code: int
+    error: bool
+    message: string
+
+  let actual = MyJson(code: 400, error: false, message: "test")
+  let expected = """{"code":400,"error":false,"message":"test"}"""
+
+  # ToJsonString needs an actual object, it will not work with a JsonNode (%*{})
+  check actual.toJsonString() == expected
+
+test "Parse a valid json body from a response":
+  let response = Response(status: 200, body: """{"key": "value"}""")
+
+  let actual = response.json()
+  let expected = %*{"key": "value"}
+
+  check actual == expected
+
+test "Parsing an invalid json body from a respones raises an error":
+  let response = Response(status: 200, body: """{"key": "value""")
+
+  expect JsonParsingError:
+    discard response.json()
+
+test "Parse a valid json body and unmarshal it":
+  type MyJson = object
+    code: int
+    error: bool
+    message: string
+
+  let response = Response(status: 200, body: """{"code": 200, "error": false, "message": "test"}""")
+
+  let actual = MyJson(code: 200, error: false, message: "test")
+  let expected = response.to(MyJson)
+
+  check actual == expected
+  check actual is MyJson
